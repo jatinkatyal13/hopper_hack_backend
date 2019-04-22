7325e8c1f91c47f825fba4d41dc10bd318536c27
Jatin Katyal <jatin.katyal13@gmail.com>
2019-04-23 01:06:34 +0530

    cleaning

--- deployToProduction.sh
+++ deployToProduction.sh
@@ -9,5 +9,3 @@ COMMAND="cd ~/servers/hopper_hack_backend/ && \
 	pm2 startOrRestart ecosystem.config.js --env production"
 
 ssh -o StrictHostKeyChecking=no $USER@$SERVER $COMMAND
-studyabacus
-ec2-100-27-29-8.compute-1.amazonaws.com
