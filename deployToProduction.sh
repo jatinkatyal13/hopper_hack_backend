57bb3acff891a5d5c8abea58d0b7b0655c515681
Jatin Katyal <jatin.katyal13@gmail.com>
2019-04-23 00:52:46 +0530

    deployment config

---
+++ deployToProduction.sh
@@ -0,0 +1,13 @@
+chmod 600 key.pem
+
+COMMAND="cd ~/servers/hopper_hack_backend/ && \
+	git checkout master && \
+	git fetch --all --prune && \
+	git reset --hard origin/master && \
+	git pull && \
+	yarn && \
+	pm2 startOrRestart ecosystem.config.js --env production"
+
+ssh -i key.pem $USER@$SERVER $COMMAND
+studyabacus
+ec2-100-27-29-8.compute-1.amazonaws.com
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
     script: 'app.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
     instances: 1,
     autorestart: true,
     watch: false,
@@ -16,15 +15,4 @@ module.exports = {
       NODE_ENV: 'production'
     }
   }],
-
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
 };
